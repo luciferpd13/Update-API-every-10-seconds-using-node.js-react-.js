0ca6485c10c4065e329622bab741e8aec762f468
Prasanna <prasanna.dubey1997@gmail.com>
2019-04-13 23:32:43 +0530

    API Updation

---
+++ socket-io-client/build/precache-manifest.1e6492433fc33ef7395444878af7f85b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "33aa098331793e28f944",
+    "url": "/static/css/main.23acbab4.chunk.css"
+  },
+  {
+    "revision": "33aa098331793e28f944",
+    "url": "/static/js/main.b5a0825e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8413a730b9d7ce18660c",
+    "url": "/static/js/2.c021fb19.chunk.js"
+  },
+  {
+    "revision": "beedcb9e4a9f998c6b822b5d8285806f",
+    "url": "/index.html"
+  }
+];
